dfd7365e63d17fadfa5238091242f1fb11220f77
HanaLouizaMoussaoui <hanalouizam@gmail.com>
2024-11-13 12:26:10 -0500

    Setup ASP.Net Core Identity and Scaffolding Identity files

---
+++ HPlusSport.Security.Identity/Pages/Privileged.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace HPlusSport.Security.Identity.Pages
+{
+    public class PrivilegedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
