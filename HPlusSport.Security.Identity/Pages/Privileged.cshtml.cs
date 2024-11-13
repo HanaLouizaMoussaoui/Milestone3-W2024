4c09349ed94709c774208df57540d21a78b732fd
HanaLouizaMoussaoui <hanalouizam@gmail.com>
2024-11-13 12:28:19 -0500

    Added authorization

--- HPlusSport.Security.Identity/Pages/Privileged.cshtml.cs
+++ HPlusSport.Security.Identity/Pages/Privileged.cshtml.cs
@@ -1,8 +1,10 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace HPlusSport.Security.Identity.Pages
 {
+    [Authorize]
     public class PrivilegedModel : PageModel
     {
         public void OnGet()
